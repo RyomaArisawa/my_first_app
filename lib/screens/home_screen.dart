6fd7dc433e25b923837add13e715b2904c2551bb
RyomaArisawa <ryoma.arisawa@isub.co.jp>
2022-03-25 22:00:40 +0900

    created forms

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -8,6 +8,10 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  String _textHeader = "あなたの名前を教えてください";
+  TextEditingController _textEditingController = TextEditingController();
+  Widget _imageWidget = Container();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -15,6 +19,43 @@ class _HomeScreenState extends State<HomeScreen> {
         title: Text("初めてのアプリ"),
         centerTitle: true,
       ),
+      body: Container(
+        padding: EdgeInsets.all(8.0),
+        child: Column(
+          children: [
+            Center(
+                child: Text(
+              _textHeader,
+              style: TextStyle(fontSize: 20.0),
+            )),
+            TextField(
+              keyboardType: TextInputType.text,
+              style: TextStyle(fontSize: 20.0),
+              controller:
+                  _textEditingController, // 入力した文字を管理するTextEditingController
+            ),
+            Expanded(child: _imageWidget),
+            SizedBox(
+              width: double.infinity,
+              child: ElevatedButton(
+                onPressed: () => _onClick(),
+                child: Text(
+                  "押してね",
+                  style: TextStyle(fontSize: 20.0),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
     );
+  }
+
+  _onClick() {
+    setState(() {
+      var inputText = _textEditingController.text;
+      _textHeader = "こんにちは$inputTextさん";
+      _imageWidget = Image.asset("assets/images/hello.png");
+    });
   }
 }
